240f642e9982dd658a9be2b9c2bccf76b187ca00
trevor020601 <trevorkarl5@gmail.com>
2025-10-18 11:26:17 -0500

    Setting up Entity Framework Core (configuring for SQL Server)

---
+++ BloodPressureApp/SharedDataSource/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SharedDataSource;
+
+public class ApplicationDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
