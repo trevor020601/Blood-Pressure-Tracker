2bc0ab45f8d58b2e827cf7d10a03ccb0d8cfee30
trevor020601 <trevorkarl5@gmail.com>
2025-11-04 20:16:12 -0600

    Removing .NET MAUI project due to pipeline issues (for now...)

--- BloodPressureApp/BloodPressureApp/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "BloodPressureApp")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "BloodPressureApp.Pages")]
--- BloodPressureApp/BloodPressureApp/Views/Login.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace BloodPressureApp.Views;
-
-public partial class Login : ContentPage
-{
-	public Login()
-	{
-		InitializeComponent();
-	}
-}
