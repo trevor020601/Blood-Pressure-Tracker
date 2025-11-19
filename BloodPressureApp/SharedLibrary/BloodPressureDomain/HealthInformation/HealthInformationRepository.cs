05af3dcca668b6b51aedf9d5e6431f44b42bb61e
trevor020601 <trevorkarl5@gmail.com>
2025-11-18 20:57:11 -0600

    HealthInformation repository (no time to code tonight)

---
+++ BloodPressureApp/SharedLibrary/BloodPressureDomain/HealthInformation/HealthInformationRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SharedLibrary.Attributes;
+
+namespace SharedLibrary.BloodPressureDomain.HealthInformation;
+
+[InjectDependency(ServiceLifetime.Scoped)]
+public interface IHealthInformationRepository
+{
+
+}
+
+public sealed class HealthInformationRepository : IHealthInformationRepository
+{
+}
